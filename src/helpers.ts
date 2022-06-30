d8ce0955f9b5ccf5ae545ce8f15cb38654abdbe5
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2022-06-30 14:54:08 +0530

    update subgraph

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,25 +3,33 @@ import { RiskPool } from '../generated/RiskPool/RiskPool';
 import { InsureDayData, Owner, PolicyInfo, PoolInfo } from '../generated/schema';
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
-export const RISK_POOL_ADDRESS = '0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0';
+export const RISK_POOL_ADDRESS = '0x8E890C091382051F3947b227072cAc14c00c9E8D';
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
 export let ZERO_BD = BigDecimal.fromString('0');
 export let ONE_BD = BigDecimal.fromString('1');
 
 class PoolData {
-  utilizationRate: ethereum.CallResult<BigInt>;
-  poolPrice: ethereum.CallResult<BigInt>;
+  utilizationRate: BigDecimal;
+  poolPrice: BigInt;
 }
 
-export function getOrCreatePool(): PoolInfo {
+export function bigIntExp18(): BigInt {
+  return BigInt.fromString('1000000000000000000');
+}
+
+export function bigDecimalExp18(): BigDecimal {
+  return BigDecimal.fromString('1000000000000000000');
+}
+
+export function createPool(): PoolInfo {
   let pool = PoolInfo.load(RISK_POOL_ADDRESS);
   if (pool === null) {
     pool = new PoolInfo(RISK_POOL_ADDRESS);
     pool.totalLiquidity = ZERO_BI;
     pool.availableLiquidity = ZERO_BI;
     pool.lockedAmount = ZERO_BI;
-    pool.utilizationRate = ZERO_BI;
+    pool.utilizationRate = ZERO_BD;
     pool.poolPrice = ZERO_BI;
     pool.policyCount = 0;
     pool.paused = false;
@@ -29,7 +37,7 @@ export function getOrCreatePool(): PoolInfo {
   return pool;
 }
 
-export function getOrCreateUser(address: Address): Owner {
+export function createUser(address: Address): Owner {
   let user = Owner.load(address.toHexString());
   if (user === null) {
     user = new Owner(address.toHexString());
@@ -41,17 +49,17 @@ export function getOrCreateUser(address: Address): Owner {
   return user;
 }
 
-export function getOrCreatePolicy(policyId: BigInt): PolicyInfo {
+export function createPolicy(policyId: BigInt): PolicyInfo {
   let policy = PolicyInfo.load(policyId.toHex());
   if (policy === null) {
     policy = new PolicyInfo(policyId.toHex());
     policy.policyId = policyId;
     policy.asset = '';
+    policy.assetPrice = ZERO_BI;
     policy.owner = ADDRESS_ZERO;
     policy.payOutAmount = ZERO_BI;
     policy.premium = ZERO_BI;
     policy.endTime = ZERO_BI;
-    policy.threshold = ZERO_BI.toI32();
     policy.startTime = ZERO_BI;
     policy.utilized = false;
   }
@@ -60,22 +68,22 @@ export function getOrCreatePolicy(policyId: BigInt): PolicyInfo {
 
 export function getUtilRateAndPoolPrice(poolAddress: Address): PoolData {
   const pool = RiskPool.bind(poolAddress);
-  const utilizationRate = pool.try_utilizationRate();
-  const poolPrice = pool.try_poolPrice();
+  const utilizationRate = pool.utilizationRate().divDecimal(bigDecimalExp18());
+  const poolPrice = pool.poolPrice();
   return { utilizationRate, poolPrice };
 }
 
-export function getTotalLiquidity(poolAddress: Address): ethereum.CallResult<BigInt> {
+export function getTotalLiquidity(poolAddress: Address): BigInt {
   const pool = RiskPool.bind(poolAddress);
-  return pool.try_totalLiquidity();
+  return pool.totalLiquidity();
 }
 
-export function getOrCreateInsureDayData(event: ethereum.Event): InsureDayData {
+export function createInsureDayData(event: ethereum.Event): InsureDayData {
   let timestamp = event.block.timestamp.toI32();
   let dayID = timestamp / 86400;
   let dayStartTimestamp = dayID * 86400;
   let insureDayData = InsureDayData.load(dayID.toString());
-  if (InsureDayData === null) {
+  if (insureDayData === null) {
     insureDayData = new InsureDayData(dayID.toString());
     insureDayData.date = dayStartTimestamp;
     insureDayData.dailyVolume = ZERO_BI;
--- src/mappings.ts
+++ src/mappings.ts
@@ -1,114 +1,115 @@
-import { Address, BigInt } from '@graphprotocol/graph-ts';
+import { Address } from '@graphprotocol/graph-ts';
 import { Deposit, Insured, PolicyPaid, Unlocked, Withdraw, WithdrawRequested } from '../generated/RiskPool/RiskPool';
 import { Transfer } from '../generated/Vault/Vault';
-import { PolicyInfo } from './../generated/schema';
 import {
-  getOrCreatePool,
-  getOrCreateUser,
-  getOrCreatePolicy,
+  createPool,
+  createUser,
+  createPolicy,
   getUtilRateAndPoolPrice,
   RISK_POOL_ADDRESS,
   getTotalLiquidity,
-  getOrCreateInsureDayData,
+  createInsureDayData,
+  ZERO_BI,
+  bigIntExp18,
 } from './helpers';
 
 export function handleDeposit(event: Deposit): void {
-  let pool = getOrCreatePool();
+  let pool = createPool();
   pool.totalLiquidity = pool.totalLiquidity.plus(event.params.amount);
   pool.availableLiquidity = pool.totalLiquidity.minus(pool.lockedAmount);
   const poolData = getUtilRateAndPoolPrice(Address.fromString(RISK_POOL_ADDRESS));
-  pool.utilizationRate = poolData.utilizationRate.reverted ? pool.utilizationRate : poolData.utilizationRate.value;
-  pool.poolPrice = poolData.poolPrice.reverted ? pool.poolPrice : poolData.poolPrice.value;
+  pool.utilizationRate = poolData.utilizationRate;
+  pool.poolPrice = poolData.poolPrice;
   pool.save();
 
-  let depositor = getOrCreateUser(event.params.depositor);
+  let depositor = createUser(event.params.depositor);
   depositor.liquidityProvided = depositor.liquidityProvided.plus(event.params.amount);
   depositor.save();
 }
 
 export function handleWithdrawRequested(event: WithdrawRequested): void {
-  const withdrawer = getOrCreateUser(event.params.withdrawer);
+  const withdrawer = createUser(event.params.withdrawer);
   withdrawer.unlockAmount = event.params.amount;
   withdrawer.unlockTimestamp = event.params.unlockTime;
   withdrawer.save();
 }
 
 export function handleWithdraw(event: Withdraw): void {
-  const pool = getOrCreatePool();
+  const pool = createPool();
   pool.totalLiquidity = pool.totalLiquidity.minus(event.params.amount);
   pool.availableLiquidity = pool.totalLiquidity.minus(pool.lockedAmount);
   const poolData = getUtilRateAndPoolPrice(Address.fromString(RISK_POOL_ADDRESS));
-  pool.utilizationRate = poolData.utilizationRate.reverted ? pool.utilizationRate : poolData.utilizationRate.value;
-  pool.poolPrice = poolData.poolPrice.reverted ? pool.poolPrice : poolData.poolPrice.value;
+  pool.utilizationRate = poolData.utilizationRate;
+  pool.poolPrice = poolData.poolPrice;
   pool.save();
 }
 
 export function handleInsured(event: Insured): void {
-  const pool = getOrCreatePool();
+  const pool = createPool();
   const totalLiquidity = getTotalLiquidity(Address.fromString(RISK_POOL_ADDRESS));
-  pool.totalLiquidity = totalLiquidity.reverted ? pool.totalLiquidity : totalLiquidity.value;
+  pool.totalLiquidity = totalLiquidity;
   pool.lockedAmount = pool.lockedAmount.plus(event.params.payOutAmount);
   pool.availableLiquidity = pool.totalLiquidity.minus(pool.lockedAmount);
   const poolData = getUtilRateAndPoolPrice(Address.fromString(RISK_POOL_ADDRESS));
-  pool.utilizationRate = poolData.utilizationRate.reverted ? pool.utilizationRate : poolData.utilizationRate.value;
-  pool.poolPrice = poolData.poolPrice.reverted ? pool.poolPrice : poolData.poolPrice.value;
+  pool.utilizationRate = poolData.utilizationRate;
+  pool.poolPrice = poolData.poolPrice;
   pool.policyCount = pool.policyCount + 1;
   pool.save();
 
-  let insured = getOrCreateUser(event.params.insured);
+  let insured = createUser(event.params.insured);
   insured.policyCount = insured.policyCount + 1;
   insured.save();
 
-  const policy = new PolicyInfo(event.params.policyId.toHex());
+  const policy = createPolicy(event.params.policyId);
   policy.policyId = event.params.policyId;
   policy.asset = event.params.asset;
+  policy.assetPrice = event.params.currentAssetPrice;
   policy.owner = insured.id;
   policy.payOutAmount = event.params.payOutAmount;
   policy.premium = event.params.premium.plus(event.params.settelmentFee);
   policy.endTime = event.params.endTime;
-  policy.threshold = event.params.threshold.toI32();
   policy.startTime = event.block.timestamp;
   policy.utilized = false;
   policy.save();
 
-  const insureDayData = getOrCreateInsureDayData(event);
+  const insureDayData = createInsureDayData(event);
   insureDayData.dailyVolume = insureDayData.dailyVolume.plus(event.params.premium);
   insureDayData.save();
 }
 
 export function handlePolicyPaid(event: PolicyPaid): void {
-  const policy = getOrCreatePolicy(event.params.policyId);
+  const policy = createPolicy(event.params.policyId);
   policy.utilized = true;
   policy.save();
 
-  const pool = getOrCreatePool();
+  const pool = createPool();
   pool.totalLiquidity = pool.totalLiquidity.minus(policy.payOutAmount);
   pool.lockedAmount = pool.lockedAmount.minus(policy.payOutAmount);
   pool.availableLiquidity = pool.totalLiquidity.minus(pool.lockedAmount);
   const poolData = getUtilRateAndPoolPrice(Address.fromString(RISK_POOL_ADDRESS));
-  pool.utilizationRate = poolData.utilizationRate.reverted ? pool.utilizationRate : poolData.utilizationRate.value;
-  pool.poolPrice = poolData.poolPrice.reverted ? pool.poolPrice : poolData.poolPrice.value;
+  pool.utilizationRate = poolData.utilizationRate;
+  pool.poolPrice = poolData.poolPrice;
   pool.save();
 }
 
 export function handleUnlocked(event: Unlocked): void {
-  const pool = getOrCreatePool();
+  const pool = createPool();
   pool.totalLiquidity = pool.totalLiquidity.minus(event.params.amountUnlocked);
   pool.lockedAmount = pool.lockedAmount.minus(event.params.amountUnlocked);
   pool.availableLiquidity = pool.totalLiquidity.minus(pool.lockedAmount);
   const poolData = getUtilRateAndPoolPrice(Address.fromString(RISK_POOL_ADDRESS));
-  pool.utilizationRate = poolData.utilizationRate.reverted ? pool.utilizationRate : poolData.utilizationRate.value;
-  pool.poolPrice = poolData.poolPrice.reverted ? pool.poolPrice : poolData.poolPrice.value;
+  pool.utilizationRate = poolData.utilizationRate;
+  pool.poolPrice = poolData.poolPrice;
   pool.save();
 }
 
 export function handlePaused(): void {
-  const pool = getOrCreatePool();
+  const pool = createPool();
   pool.paused = true;
 }
 
 export function handleUnpaused(): void {
-  const pool = getOrCreatePool();
+  const pool = createPool();
   pool.paused = false;
 }
 
@@ -116,12 +117,17 @@ export function handleTransfer(event: Transfer): void {
   if (event.params.from == Address.zero()) {
     return;
   }
-  const pool = getOrCreatePool();
-  const amountUSDC = event.params.value.times(pool.poolPrice).div(BigInt.fromString('1000000000000000000'));
-  const fromUser = getOrCreateUser(event.params.from);
-  fromUser.liquidityProvided = fromUser.liquidityProvided.minus(amountUSDC);
+  const pool = createPool();
+  const amountUSDC = event.params.value.times(pool.poolPrice).div(bigIntExp18());
+  const fromUser = createUser(event.params.from);
+  fromUser.liquidityProvided = fromUser.liquidityProvided.minus(amountUSDC).lt(ZERO_BI)
+    ? ZERO_BI
+    : fromUser.liquidityProvided.minus(amountUSDC);
   fromUser.save();
-  const toUser = getOrCreateUser(event.params.to);
-  toUser.liquidityProvided = toUser.liquidityProvided.plus(amountUSDC);
-  toUser.save();
+
+  if (event.params.to != Address.zero()) {
+    const toUser = createUser(event.params.to);
+    toUser.liquidityProvided = toUser.liquidityProvided.plus(amountUSDC);
+    toUser.save();
+  }
 }
